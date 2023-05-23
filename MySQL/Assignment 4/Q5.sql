46dda6377cb3fb55a17ece02dcfe9d7b5874aeba
TUSHARP0 <130496086+TUSHARP0@users.noreply.github.com>
2023-05-23 07:23:03 +0530

    Create Q5.sql

---
+++ MySQL/Assignment 4/Q5.sql
@@ -0,0 +1,12 @@
+/* write a procedure to display all pid,pname,cid,cname and salesman name */
+
+Delimiter //
+create procedure dis_prod(in ppid int)
+Begin
+Select p.pid,p.pname,c.cid,c.cname,s.sname
+from product p, category2 c, salesman2 s
+where p.cid=c.cid and p.sid=s.sid and p.pid=ppid;
+End //
+Delimiter ;
+
+call dis_prod(111)
