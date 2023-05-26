d94fc7490833e618474b352b2edf68815dbbf595
TUSHARP0 <130496086+TUSHARP0@users.noreply.github.com>
2023-05-26 14:32:27 +0530

    Create Q7.sql

---
+++ MySQL/Assignment 4/Q7.sql
@@ -0,0 +1,42 @@
+/* Write a procedure that displays the following information of all emp
+Empno,Name,job,Salary,Status,deptno
+Note: - Status will be (Greater, Lesser or Equal) respective to average salary of their own
+department. */
+
+
+
+Delimiter //
+create procedure disp_avg1()
+Begin
+Declare finish int default 0;
+Declare status varchar(20);
+Declare average double(9,2);
+Declare vempno,vmgr,vdeptno int;
+Declare vename,vjob varchar(20);
+Declare vhiredate date;
+Declare vsal,vcomm double(9,2);
+Declare emp_cur cursor for select * from emp;
+Declare continue handler for NOT FOUND set finish = 1;
+open emp_cur;
+l1 : loop
+fetch emp_cur into vempno,vename,vjob,vmgr,vhiredate,vsal,vcomm,vdeptno;
+if finish =1 then
+leave l1;
+end if;
+select avg(sal) into average
+from emp;
+if average > vsal then
+set status = 'Greater';
+elseif average < vsal then
+set status = 'Lesser';
+elseif average = vsal then
+set status = 'Equal';
+else
+set status = "Emp table is empty";
+end if;
+select vempno,vename,vjob,vsal,status,vdeptno;
+end loop;
+close emp_cur;
+End //
+Delimiter ;
+
