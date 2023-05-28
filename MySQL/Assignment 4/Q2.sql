881c9d9fb3d2fc388a2f38866ed166cb52fd726f
TUSHARP0 <130496086+TUSHARP0@users.noreply.github.com>
2023-05-28 06:49:16 +0530

    Update Q2.sql

--- MySQL/Assignment 4/Q2.sql
+++ MySQL/Assignment 4/Q2.sql
@@ -1,7 +1,3 @@
-/* write a procedure to delete record from employee table.
-the procedure should accept empno as input parameter.
-write delete statement inside procedure delete_emp to delete one record from emp
-table */
 
 Delimiter //
 create procedure delete_rec(in no int)
